008803eb7f46bc30c27a7854891ed254b96ffe69
Zaid-maker <pzhafeez@gmail.com>
2022-08-29 22:56:57 +0500

    ready for production and middlewares

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,17 +1,32 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-        config: {
-            provider: 'mailgun',
-            providerOptions: {
-                key: env('MAILGUN_API_KEY'), 
-                domain: env('MAILGUN_DOMAIN'), 
-                //url: env('MAILGUN_URL', 'https://api.mailgun.net')
-            },
-            settings: {
-                defaultForm: 'no-reply@devmirza.ml',
-                defaultReplyTo: 'no-reply@devmirza.ml'
-            }
-        }
-    }
-})
+  // ...
+  email: {
+    config: {
+      provider: "mailgun",
+      providerOptions: {
+        key: env("MAILGUN_API_KEY"),
+        domain: env("MAILGUN_DOMAIN"),
+        //url: env('MAILGUN_URL', 'https://api.mailgun.net')
+      },
+      settings: {
+        defaultForm: "no-reply@devmirza.ml",
+        defaultReplyTo: "no-reply@devmirza.ml",
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
