d7bd857df9935f1bb0476cd6cdcc153ba9a6fab0
Zaid-maker <pzhafeez@gmail.com>
2022-08-30 16:01:13 +0500

    yeah

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,18 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-      config: {
-        provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'pzhafeez@gmail.com',
-          defaultReplyTo: 'pzhafeez@gmail.com',
-          testAddress: 'pzhafeez@gmail.com',
-        },
+  // ...
+  email: {
+    config: {
+      provider: 'mailgun',
+      providerOptions: {
+        key: env('MAILGUN_API_KEY'), // Required
+        domain: env('MAILGUN_DOMAIN'), // Required
+        //url: env('MAILGUN_URL', 'https://api.mailgun.net'), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+      },
+      settings: {
+        defaultFrom: 'no-reply@devmirza.ml',
+        defaultReplyTo: 'no-reply@devmirza.ml',
       },
     },
-    // ...
-  });
+  },
+  // ...
+});
