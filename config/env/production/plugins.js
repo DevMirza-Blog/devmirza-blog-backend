866772367c153a1469a7f8130a079c1d1f41017a
Zaid-maker <pzhafeez@gmail.com>
2022-08-30 16:21:31 +0500

    ahhh

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
             providerOptions: {
                 key: env('MAILGUN_API_KEY'), 
                 domain: env('MAILGUN_DOMAIN'), 
-                url: env('MAILGUN_URL', 'https://api.mailgun.net')
+                //url: env('MAILGUN_URL', 'https://api.mailgun.net')
             },
             settings: {
-                defaultForm: 'pzhafeez@gmail.com',
-                defaultReplyTo: 'pzhafeez@gmail.com'
+                defaultForm: 'no-reply@devmirza.ml',
+                defaultReplyTo: 'no-reply@devmirza.ml'
             }
         }
     }
