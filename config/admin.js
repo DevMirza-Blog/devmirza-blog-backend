a13ae0e4d684ecd7a7c44aedd88ee61ea1d48e8f
Zaid-maker <pzhafeez@gmail.com>
2022-08-31 21:57:55 +0500

    Update deps

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,15 @@
+const forgotPasswordTemplate = require('./email-templates/forget-password');
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
+  },
+  forgotPassword: {
+    from: 'support@devmirza.ml',
+    replyTo: 'support@devmirza.ml',
+    emailTemplate: forgotPasswordTemplate,
   },
 });
