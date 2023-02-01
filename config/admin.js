52b91274c257fb11dc39fb37ed04735545be96dc
Zaid-maker <pzhafeez@gmail.com>
2023-02-01 16:21:50 +0500

    format

--- config/admin.js
+++ config/admin.js
@@ -1,15 +1,15 @@
-const forgotPasswordTemplate = require('./email-templates/forget-password');
+const forgotPasswordTemplate = require("./email-templates/forget-password");
 
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   forgotPassword: {
-    from: 'support@devmirza.ml',
-    replyTo: 'support@devmirza.ml',
+    from: "support@devmirza.ml",
+    replyTo: "support@devmirza.ml",
     emailTemplate: forgotPasswordTemplate,
   },
 });
