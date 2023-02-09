73520e137e9ace97fe843d51f3df24f11a438101
DevMirza <pzhafeez@gmail.com>
2023-02-09 09:18:42 +0000

    mailgun

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,18 +1,5 @@
 module.exports = ({ env }) => ({
   // ...
   ckeditor: true,
-  email: {
-    config: {
-      provider: "mailgun",
-      providerOptions: {
-        key: env("MAILGUN_API_KEY"),
-        domain: env("MAILGUN_DOMAIN"),
-        //url: env('MAILGUN_URL', 'https://api.mailgun.net')
-      },
-      settings: {
-        defaultForm: "no-reply@devmirza.ml",
-        defaultReplyTo: "no-reply@devmirza.ml",
-      },
-    },
-  },
+  // ...
 });
--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,5 @@
 module.exports = ({ env }) => ({
   // ...
   ckeditor: true,
-  email: {
-    config: {
-      provider: "mailgun",
-      providerOptions: {
-        key: env("MAILGUN_API_KEY"), // Required
-        domain: env("MAILGUN_DOMAIN"), // Required
-        //url: env('MAILGUN_URL', 'https://api.mailgun.net'), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
-      },
-      settings: {
-        defaultFrom: "no-reply@devmirza.ml",
-        defaultReplyTo: "no-reply@devmirza.ml",
-      },
-    },
-  },
   // ...
 });
